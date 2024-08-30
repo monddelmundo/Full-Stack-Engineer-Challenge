f891dff44089bdbdd97234ec77de66df85cea58c
Mond <edmond.delmundo@gmail.com>
2024-08-30 13:56:11 +0800

    Removed console log of password

--- api/src/instances/sequelize.ts
+++ api/src/instances/sequelize.ts
@@ -3,7 +3,6 @@ import { Sequelize } from "sequelize";
 const db = 'postgres'
 const username = 'postgres'
 
-console.log("Password", process.env.POSTGRES_PASSWORD)
 export const sequelize = new Sequelize(db, username, process.env.POSTGRES_PASSWORD ?? '', {
   dialect: "postgres",
   port: 5432,
